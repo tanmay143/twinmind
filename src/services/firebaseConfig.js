b0487c3490b10a0d2366928b01d4982eba5f2efe
tanmay143 <tanmaypajgade@gmail.com>
2025-06-03 01:58:12 -0400

    updated config with api

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -8,7 +8,15 @@ import { getAuth, GoogleAuthProvider, signInWithCredential } from 'firebase/auth
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
+export const firebaseConfig = {
+  apiKey: "AIzaSyCLcM14N2xRqG4Uhj8VhYrvJ8mtlwZfNrc",
+  authDomain: "twinmind-94d5d.firebaseapp.com",
+  projectId: "twinmind-94d5d",
+  storageBucket: "twinmind-94d5d.firebasestorage.app",
+  messagingSenderId: "11230593000",
+  appId: "1:11230593000:web:96dd2f8a7d7701f3616e5d",
+  measurementId: "G-FSL0SDJ3T9"
+};
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
